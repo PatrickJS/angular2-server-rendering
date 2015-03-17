495d588fbe98ba6d2c9f190d74e12f60cfbb6646
gdi2290 <github@gdi2290.com>
2015-03-17 01:53:49 -0700

    chore: src/

---
+++ src/app.es6.js
@@ -0,0 +1,39 @@
+import {Component, Template} from 'angular2/angular2';
+import {If} from 'angular2/directives';
+import {bootstrap} from 'angular2/angular2';
+
+
+function dedent(strings, ...values) {
+  var result = '';
+  for (var i = 0; i < strings.length; i++) {
+    result += strings[i].replace(/\n\s+/g, '\n') + values[i];
+  }
+  return result;
+}
+
+export var template = dedent`
+  <span *if="name">
+    Hello, {{ name }}!
+  </span>
+`;
+
+@Component({
+  selector: 'app'
+})
+@Template({
+  inline: template,
+  directives: [
+    If
+  ]
+})
+export class App {
+  constructor() {
+    this.name = 'World';
+    setTimeout(() => {
+      this.name = 'NEW World'
+    }, 2000);
+  }
+  swag() {
+    console.log('swag')
+  }
+}

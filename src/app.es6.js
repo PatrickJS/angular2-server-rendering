045a8d6ee9c1c8d8ccb17c587b173b632448a25a
gdi2290 <github@gdi2290.com>
2015-03-17 02:16:50 -0700

    fix(app): dedent

--- src/app.es6.js
+++ src/app.es6.js
@@ -6,10 +6,17 @@ import {bootstrap} from 'angular2/angular2';
 function dedent(strings, ...values) {
   var result = '';
   for (var i = 0; i < strings.length; i++) {
-    result += strings[i].replace(/\n\s+/g, '\n') + values[i];
+    result += (strings[i]) ?
+      strings[i].replace(/\n\s+/g, '\n') +
+      (
+        values[i] ? values[i] : ''
+      )
+    : '';
+    console.log('result '+i, result);
   }
   return result;
 }
+
 
 export var template = `
   <span *if="name">

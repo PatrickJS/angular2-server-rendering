77c8297a2825c4c6ae960278770ef3e1eb8f9eb0
gdi2290 <github@gdi2290.com>
2015-03-17 01:53:28 -0700

    chore: index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+var port = process.env.PORT    || 3000;
+// var ssl  = process.env.SSLPORT || 4000;
+
+// Module dependencies
+var http = require('http');
+// var https = require('https');
+
+/*
+var options = {
+  key:  fs.readFileSync('/private/etc/apache2/ssl/ssl.key'),
+  cert: fs.readFileSync('/private/etc/apache2/ssl/ssl.crt')
+};
+*/
+require('gulp-traceur/node_modules/traceur/bin/traceur-runtime.js');
+var server = require('./server/app_server')(__dirname);
+
+// Start server
+http.createServer(server).listen(port, function() {
+  console.log('Listening on port: ' + port + ' in ' + process.env.NODE_ENV);
+});
+/*
+https.createServer(options, server).listen(ssl, function() {
+  console.log('Listening on port: ' + ssl + ' in ' + process.env.NODE_ENV);
+});
+*/

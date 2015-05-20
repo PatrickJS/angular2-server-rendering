6dcb7171751e297e7c78bf6a7df6338d25f582f9
gdi2290 <github@gdi2290.com>
2015-05-20 11:22:15 -0700

    refactor(ng2Engine): use ts and use bootstrap

---
+++ src/ng2Engine/angular2.server.ts
@@ -0,0 +1,51 @@
+// Zone.js
+function HTMLElement() {
+}
+HTMLElement.prototype.onclick = function(fn) {
+  fn()
+};
+
+function Element() {
+}
+Element.prototype.onclick = function(fn) {
+  fn()
+};
+
+function XMLHttpRequest() {
+}
+
+function WebSocket() {
+}
+
+var document = {
+  createElement: function() {
+    return new Element();
+  }
+}
+
+global.HTMLElement = HTMLElement;
+global.Element = Element;
+global.document = document;
+global.XMLHttpRequest = XMLHttpRequest;
+global.WebSocket = WebSocket;
+global.window = global;
+
+// typescript decoratos
+import 'reflect-metadata';
+
+// legacy angular22
+import 'angular2/node_modules/traceur/bin/traceur-runtime';
+
+// angular2 assert
+import * as assert 'rtts_assert/rtts_assert';
+
+global.assert = global.assert || assert;
+global.assert.define = global.assert.define || function() {};
+
+// dom closure hack
+import {Parse5DomAdapter} from 'angular2/src/dom/parse5_adapter';
+Parse5DomAdapter.makeCurrent();
+
+// npm build
+import 'angular2/angular2';
+

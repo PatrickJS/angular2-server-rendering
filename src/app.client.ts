36b3b6fbd290a21be83b68703de1e7f0c881830e
gdi2290 <github@gdi2290.com>
2015-05-22 12:51:47 -0700

    fix(app.client): ensure BrowserDomAdapter

--- src/app.client.ts
+++ src/app.client.ts
@@ -1,14 +1,22 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
 
-import {bind} from 'angular2/angular2';
+// needed because dom_renderer uses DOM
+import {BrowserDomAdapter} from 'angular2/src/dom/browser_adapter';
+BrowserDomAdapter.makeCurrent();
+
+// import {bootstrap} from 'angular2/angular2';
 import {bootstrap} from './angular2_client/bootstrap.client';
-import {OpaqueToken} from 'angular2/di';
+import {bind, OpaqueToken} from 'angular2/di';
+
 import {SERVER_RENDERED_TOKEN} from './angular2_client/dom_renderer';
 import {App} from './app/app';
+
 import {ShadowDomStrategy, NativeShadowDomStrategy} from 'angular2/core';
 
+
 bootstrap(App, [
-  bind(ShadowDomStrategy).toClass(NativeShadowDomStrategy),
+  // doesn't work
+  // bind(ShadowDomStrategy).toClass(NativeShadowDomStrategy),
   bind(SERVER_RENDERED_TOKEN).toValue(true)
 ])
 .then(appRef => {

1e1e16ace803c856552605c2bdbe07aee2a22704
gdi2290 <github@gdi2290.com>
2015-05-22 14:23:47 -0700

    feat(app.client): include NativeShadowDomStrategy

--- src/app.client.ts
+++ src/app.client.ts
@@ -1,18 +1,27 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
+/// <reference path="../typings/_custom/ng2.d.ts" />
+/// <reference path="../typings/_custom/server.d.ts" />
 
-// import {bootstrap} from 'angular2/angular2';
 import {bootstrap} from './angular2_client/bootstrap.client';
+// import {bootstrap} from 'angular2/angular2';
+
+
 import {bind, OpaqueToken} from 'angular2/di';
+import {ShadowDomStrategy, NativeShadowDomStrategy} from 'angular2/core';
+import {StyleUrlResolver} from 'angular2/src/render/dom/shadow_dom/style_url_resolver';
+
 
 import {SERVER_RENDERED_TOKEN} from './angular2_client/dom_renderer';
 import {App} from './app/app';
 
-import {ShadowDomStrategy, NativeShadowDomStrategy} from 'angular2/core';
 
 
 bootstrap(App, [
   // doesn't work
-  // bind(ShadowDomStrategy).toClass(NativeShadowDomStrategy),
+  // bind(ShadowDomStrategy).toFactory(
+  //   styleUrlResolver => new NativeShadowDomStrategy(styleUrlResolver),
+  //   [StyleUrlResolver]
+  // ),
   bind(SERVER_RENDERED_TOKEN).toValue(true)
 ])
 .then(appRef => {

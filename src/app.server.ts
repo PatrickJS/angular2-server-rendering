146473541e9798256de7bafce1c5bfc5fffbe483
gdi2290 <github@gdi2290.com>
2015-05-22 12:34:21 -0700

    chore: comment out app.server

--- src/app.server.ts
+++ src/app.server.ts
@@ -1,18 +1,21 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
 
-import {bootstrap} from './angular2_server';
-import {App} from './app/app';
-
-bootstrap(App, [
-
-])
-.then(appRef => {
-  console.log('server', appRef);
-  debugger;
-  return appRef;
-});
-
-export {App};
+// DO NOT USE
 
 
-// NOT USED
+// import {bootstrap} from './angular2_server';
+// import {App} from './app/app';
+
+// bootstrap(App, [
+
+// ])
+// .then(appRef => {
+//   console.log('server', appRef);
+//   debugger;
+//   return appRef;
+// });
+
+// export {App};
+
+
+// // NOT USED

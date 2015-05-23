c63f627ad5808bcde6c2c30939b8cbca6449d5c8
gdi2290 <github@gdi2290.com>
2015-05-23 00:15:19 -0700

    fix(stringifyElement): whitelist tags

--- src/ng2Engine/stringifyElement.ts
+++ src/ng2Engine/stringifyElement.ts
@@ -6,11 +6,16 @@ import {List, ListWrapper, MapWrapper} from 'angular2/src/facade/collection';
 import {DOM} from 'angular2/src/dom/dom_adapter';
 import {isPresent, StringWrapper} from 'angular2/src/facade/lang';
 
+var singleTagWhitelist = {
+  'br': true,
+  'hr': true,
+  'input': true
+}
 
 export function stringifyElement(el): string {
   var result = '';
 
-  if (el.name && DOM.isElementNode(el) && DOM.tagName(el)) {
+  if (DOM.isElementNode(el)) {
     var tagName = StringWrapper.toLowerCase(DOM.tagName(el));
     // Opening tag
     result += '<' + tagName;
@@ -20,28 +25,32 @@ export function stringifyElement(el): string {
     var keys = ListWrapper.create();
     attributeMap.forEach((v, k) => { ListWrapper.push(keys, k); });
     ListWrapper.sort(keys);
-    for (let i = 0; i < keys.length; i++) {
-      let key = keys[i];
-      result += ' ' + key + '="' + MapWrapper.get(attributeMap, key) + '"';
+
+    if (keys.length) {
+      for (let i = 0; i < keys.length; i++) {
+        let key = keys[i];
+        result += ' ' + key + '="' + MapWrapper.get(attributeMap, key) + '"';
+      }
     }
+
     result += '>';
 
     // Children
     var children = DOM.childNodes(DOM.templateAwareRoot(el));
-    var childrenLen = children.length;
-    if (childrenLen) {
-      for (let i = 0; i < childrenLen; i++) {
+    if (children.length) {
+      for (let i = 0; i < children.length; i++) {
         result += stringifyElement(children[i]);
       }
     }
 
     // Closing tag
-    if (childrenLen) { // avoid self closing tags
+    // avoid self closing tags
+    if (!singleTagWhitelist[tagName]) {
       result += '</' + tagName + '>';
     }
   } else {
     result = DOM.getText(el);
   }
 
-  return result
+  return result;
 }

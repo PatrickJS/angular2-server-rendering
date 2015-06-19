9182d1ebd2b961058b71f850fda4026af266ed6e
gdi2290 <github@gdi2290.com>
2015-06-19 08:41:22 -0700

    chore: download files on install

--- update-ng-bundle.sh
+++ update-ng-bundle.sh
@@ -7,5 +7,12 @@ curl -o web_modules/angular2.min.js "https://code.angularjs.org/$1/angular2.min.
 curl -o web_modules/router.dev.js "https://code.angularjs.org/$1/router.dev.js"
 curl -o web_modules/router.dev.js.map "https://code.angularjs.org/$1/router.dev.js.map"
 
+curl -o web_modules/system.js  "https://raw.githubusercontent.com/systemjs/systemjs/$3/dist/system.js"
+curl -o web_modules/system.js.map  "https://raw.githubusercontent.com/systemjs/systemjs/$3/dist/system.js.map"
+curl -o web_modules/system.src.js  "https://raw.githubusercontent.com/systemjs/systemjs/$3/dist/system.src.js"
+
+curl -o web_modules/Reflect.js  "https://raw.githubusercontent.com/rbuckton/ReflectDecorators/$4/Reflect.js"
+curl -o web_modules/Reflect.js.map  "https://raw.githubusercontent.com/rbuckton/ReflectDecorators/$4/Reflect.js.map"
+
 echo "done!"
 echo ""
